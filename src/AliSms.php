40721963d3397ae05eee91f73f77ee73016b34f3
qq1415551519 <1415551519@qq.com>
2019-07-21 23:30:33 +0800

    update sendSms

--- src/AliSms.php
+++ src/AliSms.php
@@ -1,10 +1,69 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: 12155
+ * Date: 2019/7/21
+ * Time: 22:44
+ */
+
 namespace DengTp5;
+
+use AlibabaCloud\Client\AlibabaCloud;
+use AlibabaCloud\Client\Exception\ClientException;
+use AlibabaCloud\Client\Exception\ServerException;
 
 class AliSms
 {
-    public static function sendSms()
+    /**
+     * 发送短信sendSms
+     * $accessKeyId                           阿里云key
+     * $accessSecret                          阿里云Secret
+     * $mobile                                发送指定的手机号
+     * $signName                              阿里云短信签名
+     * $templateCode                          阿里云短信模板code
+     * $paramArray['code']                    验证码：code
+     * @param $paramArray
+     * @return array
+     * @throws ClientException
+     * @author:  deng    (2019/7/21 23:11)
+     */
+    public static function sendSms($paramArray)
     {
-        echo 'sms';exit;
+        $accessKeyId = $paramArray['accessKeyId'];
+        $accessSecret = $paramArray['accessSecret'];
+        $mobile = $paramArray['mobile'];
+        $signName = $paramArray['signName'];
+        $templateCode = $paramArray['templateCode'];
+        $templateParam['code'] = $paramArray['code'];
+
+        AlibabaCloud::accessKeyClient($accessKeyId, $accessSecret)
+            ->regionId('cn-hangzhou') // replace regionId as you need
+            ->asDefaultClient();
+
+        try {
+            $result = AlibabaCloud::rpc()
+                ->product('Dysmsapi')
+                // ->scheme('https') // https | http
+                ->version('2017-05-25')
+                ->action('SendSms')
+                ->method('POST')
+                ->options([
+                    'query' => [
+                        'PhoneNumbers' => $mobile,
+                        'SignName' => $signName,
+                        'TemplateCode' => $templateCode,
+                        'TemplateParam' => json_encode($templateParam)
+                    ],
+                ])
+                ->request();
+
+        } catch (ClientException $e) {
+            echo $e->getErrorMessage() . PHP_EOL;
+        } catch (ServerException $e) {
+            echo $e->getErrorMessage() . PHP_EOL;
+        }
+
+        return $result->toArray();
     }
+
 }
